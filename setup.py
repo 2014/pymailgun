dee7ce5bf32d5669223fffc733a3f3eebf15c14a
qign <dianqw@gmail.com>
2013-12-01 08:08:58 +0800

    change the setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mailgun",
+    name="pymailgun",
     version="0.0.1",
     author="fatelei",
     author_email="fatelei@gmail.com",
     description="mailgun sdk for python",
-    packages=find_packages(),
+    packages=find_packages('mailgun'),
     install_requires=["requests"],
     packages=["mailgun", "mailgun.data"]
 )
